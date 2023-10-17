ba1424d626827b1434eaff9a9db88a34060f161f
SuJaY401 <sujayhegde444@gmail.com>
2023-10-17 16:01:13 +0530

    17/10 3:38 updated

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import "../styles/Header.css";
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from"./HeaderOption.jsx";
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+
+function Header() {
+
+
+  return (
+
+          <div className="header">
+
+                      <div  className="header-left" >
+                             <img src="https://static.vecteezy.com/system/resources/previews/018/930/587/original/linkedin-logo-linkedin-icon-transparent-free-png.png" alt=" " />
+                             <div className="header-search">
+                               <SearchIcon/>
+                               <input type="text" placeholder="search"/>
+                             </div>
+
+
+                      </div>
+
+
+                      <div  className="header-right">
+                            <HeaderOption  Icon={HomeIcon} title="Home " />
+                            <HeaderOption  Icon={SupervisorAccountIcon} title="MyNetwork" />
+                            <HeaderOption  Icon={ BusinessCenterIcon } title="Jobs" />
+                            <HeaderOption  Icon={ NotificationsIcon } title="Notification"/>
+                            <HeaderOption Icon={PersonIcon} title="Profile"/>
+                            <HeaderOption  Icon={LogoutIcon} title="Logout" />
+                      </div>
+           </div>
+
+  );
+}
+
+export default Header;
