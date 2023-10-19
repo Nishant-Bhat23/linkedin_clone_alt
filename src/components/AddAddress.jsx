e8b7b4a2219b8a17a72f079d60afbccef9deec9c
Nishant-Bhat23 <nishantjbhat@gmail.com>
2023-10-19 15:55:10 +0530

    FIRST

--- src/components/AddAddress.jsx
+++ src/components/AddAddress.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/AddAddress.css'; // Import your CSS file
 
 function AddAddressPage() {
