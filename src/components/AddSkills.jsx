164aa3cf000b77cb72ff645637020e83b09444d4
SuJaY401 <sujayhegde444@gmail.com>
2023-10-19 15:16:37 +0530

    skills done

---
+++ src/components/AddSkills.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Container, TextField, Button } from '@mui/material';
+import '../styles/AddEducationPage.css';
+import  { useState } from 'react';
+
+
+function AddSkills(){
+  const [skillName, setSkillName] = useState('');
+  const [skillType, setSkillType] = useState('');
+  const [proficiency, setPorficiency] = useState('');
+
+  const handleSave = () => {
+      //saving the experience oage
+    };
+return(
+<Container maxWidth="sm" className="add-education-container">
+      <h2>ADD SKILLS</h2>
+      <div className="education-form">
+        <TextField
+          label="Skill Name"
+          fullWidth
+          variant="outlined"
+          value={skillName}
+          onChange={(e) => setSkillName(e.target.value)}
+        />
+        <TextField
+          label="Skill Type"
+          fullWidth
+          variant="outlined"
+          value={skillType}
+          onChange={(e) => setSkillType(e.target.value)}
+        />
+        <div className="form-group">
+                          <label htmlFor="proficiency" className="form-label">
+                            Proficiency Level:
+                          </label>
+                          <select
+                            id="proficiency"
+                            name="proficiency"
+                            value="outlined"
+                            onChange={(e) => setPorficiency(e.target.value)}
+                            required
+                            className="form-select"
+                          >
+                            <option value="">Select Proficiency Level</option>
+                            <option value="Beginner">Beginner</option>
+                            <option value="Intermediate">Intermediate</option>
+                            <option value="Advanced">Advanced</option>
+                          </select>
+                        </div>
+      </div><br/>
+      <Button variant="contained" color="primary" onClick={handleSave}>
+        Save
+      </Button>
+    </Container>
+
+);
+
+}
+export default AddSkills;
