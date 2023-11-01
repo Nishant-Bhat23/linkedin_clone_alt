3cde55a63fc014d1507306ca610dd390be53d562
Nishant-Bhat23 <nishantjbhat@gmail.com>
2023-11-01 19:32:05 +0530

    likes count

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,15 +1,20 @@
 // LikeButton.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IconButton } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import axios from 'axios';
 
 function LikeButton({postId,userId})  {
   const [liked, setLiked] = useState(false);
   const [likesCount, setLikesCount] = useState({});
 
-  async function handleLike(post_id, user_id) {
-     setLiked(!liked);
+useEffect(() => {
+  handleLikeCount(postId,userId);
+  console.log(likesCount)
+},[liked]);
+  async function handleLikeCount(post_id, user_id) {
+
      try {
        await axios.post("http://localhost:8080/like", {
          post_id: post_id,
@@ -26,26 +31,15 @@ function LikeButton({postId,userId})  {
        console.error(error);
      }
    }
-
-   async function handleTotalLikes(post_id) {
-       try {
-         const res = await axios.get(
-           "http://localhost:8080/" + post_id + "/totalLikes"
-         );
-         setLikesCount((prevLikes) => ({
-           ...prevLikes,
-           [post_id]: res.data,
-         }));
-       } catch (error) {
-         console.error(error);
-       }
-     }
-
+   function handleLike(){
+   setLiked(!liked);
+   }
   return (
-    <IconButton color={liked ? 'secondary' : 'default'} onClick={handleLike(postId,userId)}>
+    <IconButton color={liked ? 'secondary' : 'default'} onClick={handleLike}>
       {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+       <span>{likesCount[postId] || 0}</span>
     </IconButton>
-    <span>{likesCount[postId] || 0}</span>
+
   );
 };
 
